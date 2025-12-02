d0f5909700cf494585ceeb8f8d5cc736c16361f7
Daniel Rodrigues <danielrods2004@gmail.com>
2025-12-02 17:46:28 -0300

    Encrypting the received URL

--- main.go
+++ main.go
@@ -1,6 +1,51 @@
-package main 
+package main
 
-func main() {
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"log"
+	"net/http"
+)
 
-	
+var (
+	urlStore =  make (map[string]string)
+	secretKey = []byte("secretaeskey12345678901234567890")
+)
+
+func encrypt(originalUrl string) string {
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	plainText := []byte(originalUrl)
+	cipherText := make ([]byte, aes.BlockSize+len(plainText))
+
+	iv := cipherText[:aes.BlockSize]
+
+	if _, err := rand.Read(iv); err != nil{
+		log.Fatal(err)
+	}
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+
+	return hex.EncodeToString(cipherText)
+
+
+}
+
+func shortenUrl(w http.ResponseWriter, r *http.Request) {
+	originalUrl := r.URL.Query().Get("url")
+	if originalUrl == ""{
+		http.Error(w, "Parametro URL no quary é obrigatório", http.StatusBadRequest)
+		return
+	}
+
+	encryptedUrl := encrypt(originalUrl)
+
+
+
 }
